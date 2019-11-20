167bd198ac09d842ba4d2f83a404ca96381116fb
Samirah Amadu <amadu@student.tugraz.at>
2019-11-20 01:02:20 +0100

    Merge files

--- nn19_ex2_load.py
+++ nn19_ex2_load.py
@@ -27,6 +27,12 @@ indices_tst = np.flatnonzero((Ctst == 4) | (Ctst == 2))
 Ctst = Ctst[indices_tst]
 Xtst = Xtst[indices_tst]
 
+#########################################
+#
+# Probabilistic generative model classification
+#
+#########################################
+
 # find prior probability
 nr_training_examples = C.size
 
@@ -60,15 +66,6 @@ covariance = (examples_per_class[0]/nr_training_examples) * s[unique[0]] + \
 cov_inverse = np.linalg.pinv(covariance)
 # compute posterior probability
 sigmoid = lambda a: 1 / (1 + np.exp(-a))
-
-
-plt.figure()
-plt.xticks(np.arange(0, 19, 1))
-plt.yticks(np.arange(0, 1.1, 0.1))
-plt.ylabel('accuracy')
-plt.xlabel('features')
-plt.title('Probabilistic generative model accuracy vs number of features')
-plt.ylim([0, 1])
 
 
 def plot_point_values(x_y_array, ax, label):
@@ -111,11 +108,107 @@ def classify(X, mean, covariance, target, flag="train"):
     plot_point_values(result, plt.gca(), flag)
 
 
+######
+# PLOT
+######
+plt.figure()
+plt.xticks(np.arange(0, 19, 1))
+plt.yticks(np.arange(0, 1.1, 0.1))
+plt.ylabel('classification accuracy')
+plt.xlabel('features')
+plt.title('Probabilistic generative model accuracy vs number of features')
+plt.ylim([0, 1])
+
 classify(X, mean, covariance, C)
 classify(Xtst, mean, covariance, Ctst, flag="test")
+
 
 ax = plt.gca()
 handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles, labels)
 plt.show()
+
+#########################################
+#
+# IRLS classification
+#
+#########################################
+
+def nabla_e(X, y, target):
+    return X @ (y - target)
+
+
+def hessian(X, y):
+    R = np.diag(y[0]*(1-y[0]))
+    return X.T @ R @ X
+
+
+def update(weights, hessian, gradient):
+    return weights - (np.linalg.inv(hessian) @ gradient)
+
+
+def log_likelihood(estimate, target):
+    cls_1 = target
+    cls_2 = (1-target)
+    err_per_example = np.where(cls_1 != 0, cls_1 * np.log(estimate), 0)
+    err_per_example = np.where(cls_2 != 0, err_per_example + cls_2 * np.log(1 - estimate), 0)
+    return -np.sum(err_per_example, axis=0)
+
+
+def irls(X, target, flag="train"):
+    result = np.zeros((X.shape[1]-2, 2), dtype=float)
+    for features in range(2, X.shape[1]):
+        bias = np.ones((X.shape[0], 1))
+        input = np.append(bias, X[:, 0:features], axis=1)
+        weights = np.random.uniform(low=-0.0001, high=0.0001, size=input.shape[1])[:, np.newaxis]
+        y = sigmoid(weights.reshape((1, -1)) @ input.T)  # probability of class 1
+
+        error_old = 100
+        error_new = 0
+
+        print("--- Computing IRLS with ", features, " features.")
+        while (error_old - error_new) > 0.4:
+            error_old = error_new
+
+            # update until error converges
+            hess = hessian(input, y)
+            gradient = nabla_e(input.T, y.reshape((-1, 1)), target)
+            weights = update(weights, hess, gradient)
+
+            y = sigmoid(weights.reshape((1, -1)) @ input.T)
+            error_new = log_likelihood(y.reshape((-1, 1)), target)
+            print("error:", error_new)
+
+        probability = np.where(y > 0.5, 1, 0)
+        comparison = np.equal(probability.reshape((-1, 1)), target)
+        accuracy = np.count_nonzero(comparison)/target.size
+        print(flag, "---Nr of features ", features, " accuracy", accuracy)
+
+        result[features-2][0] = features
+        result[features-2][1] = accuracy
+
+    # plot graph
+    plt.plot(result[:, 0], result[:, 1], '+-', label=flag)
+    plot_point_values(result, plt.gca(), flag)
+
+
+######
+# PLOT
+######
+plt.figure()
+plt.xticks(np.arange(0, 19, 1))
+plt.yticks(np.arange(0, 1.1, 0.1))
+plt.ylabel('classification accuracy')
+plt.xlabel('features')
+plt.title('IRLS accuracy vs number of features')
+plt.ylim([0, 1])
+
+irls(X, np.where(C == 2, 0, 1))
+irls(Xtst, np.where(Ctst == 2, 0, 1), 'test')
+
+ax = plt.gca()
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, labels)
+plt.show()
+
 
